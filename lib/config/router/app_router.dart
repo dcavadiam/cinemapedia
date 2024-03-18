f29310c56c0e903745cbe0fe84fea605117a4435
dcavadia <dcavadia@uninorte.edu.co>
2024-03-18 16:36:26 -0500

    movie screen created

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,15 +1,27 @@
-
-
-import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
-    ),
-  ]
-);
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieID = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieID);
+          },
+        ),
+      ]),
+  GoRoute(
+    path: '/movie/:id',
+    name: MovieScreen.name,
+    builder: (context, state) {
+      final movieID = state.pathParameters['id'] ?? 'no-id';
+      return MovieScreen(movieId: movieID);
+    },
+  ),
+]);
