a0df6fefd383cdbb34af85befe8f97a31a2aa212
dcavadia <dcavadia@uninorte.edu.co>
2024-04-02 10:22:26 -0500

    add movie info provider

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieInfoProvider = StateNotifierProvider((ref) {
+  final movieRepository = ref.watch(movieRepositoryProvider);
+  return MovieMapNotifier(getMovie: movieRepository.getMovieById);
+});
+
+typedef GetMovieCallback = Future<Movie> Function(String movieID);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieID) async {
+    if (state[movieID] != null) return;
+
+    final movie = await getMovie(movieID);
+
+    state = {...state, movieID: movie};
+  }
+}
