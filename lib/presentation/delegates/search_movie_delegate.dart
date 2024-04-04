3557aa876d17cf36f90b3ceaec6152c15bc9b6c4
dcavadia <dcavadia@uninorte.edu.co>
2024-04-04 10:31:59 -0500

    add search movie delegate

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,60 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/movie.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+  @override
+  // Construir acciones
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      FadeIn(
+          animate: query.isNotEmpty,
+          duration: const Duration(milliseconds: 200),
+          child: IconButton(
+              onPressed: () => query = '', icon: const Icon(Icons.clear)))
+    ];
+  }
+
+  //Construir icono
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_rounded));
+  }
+
+  //Resultados al presionar enter
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+        );
+      },
+    );
+  }
+}
