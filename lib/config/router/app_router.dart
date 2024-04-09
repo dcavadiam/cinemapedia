cc5c3cf48adb87807c98f7f3581a1c2eb8a94c23
dcavadia <dcavadia@uninorte.edu.co>
2024-04-09 10:52:05 -0500

    Official GoRouter implemented in Custom Bottom Nav

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,19 +1,53 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-      routes: [
-        GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
+    routes: [
+      GoRoute(
+          path: "/",
           builder: (context, state) {
-            final movieID = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieID);
+            return const HomeView();
           },
-        ),
-      ]),
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieID = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieID);
+              },
+            ),
+          ]),
+      GoRoute(
+        path: "/favorites",
+        builder: (context, state) {
+          return const FavoritesView();
+        },
+      ),
+    ],
+  ),
+
+  //RUTAS PADRE/HIJO
+
+  // GoRoute(
+  //     path: '/',
+  //     name: HomeScreen.name,
+  //     builder: (context, state) => const HomeScreen(
+  //           childView: HomeView(),
+  //         ),
+  //     routes: [
+  //       GoRoute(
+  //         path: 'movie/:id',
+  //         name: MovieScreen.name,
+  //         builder: (context, state) {
+  //           final movieID = state.pathParameters['id'] ?? 'no-id';
+  //           return MovieScreen(movieId: movieID);
+  //         },
+  //       ),
+  //     ]),
 ]);
